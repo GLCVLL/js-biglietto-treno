62f3dc229960d2ab3d976f4fa363690e69b5250e
GLCVLL <g.ioele96@outlook.it>
2023-05-26 17:17:56 +0200

    get element from dom and ask user age and km

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('JS OK');
+
+// 0. RECUPERO ELEMENTO DAL DOM
+
+const element = document.getElementById('ticket-price');
+console.log(element);
+
+// CHIEDIAMO KM DA PERCORRERE E ANNI ALL'UTENTE
+
+const userKm = parseInt(prompt('quanti km vuoi percorrere?', '500'));
+const userAge = parseInt(prompt('quanti anni hai?', '18'));
+
+console.log(userKm);
+console.log(userAge);
